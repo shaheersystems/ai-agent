6bcfe63f602ed9d7d898362fae502c3f894f87c8
shaheersystems <shaheer.imran1429@gmail.com>
2025-04-21 18:50:14 +0500

    slight updates

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,15 +1,17 @@
 import { google } from "@/config/google";
-// import { ollama } from "@/config/ollama";
-import { streamText, tool } from "ai";
-// import {Gemin}
+import { streamText } from "ai";
 import { tools } from "@/ai/tools";
+
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = streamText({
     model: google("gemini-1.5-flash"),
     messages: messages,
+    system:
+      "You are a helpful assistant, who help user to find, manage and summarize day-to-day tasks",
     tools,
+    maxSteps: 2,
   });
 
   return result.toDataStreamResponse();
